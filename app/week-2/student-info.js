c05b46d1eb02d7d5319f1f29c865ca347a9f6b56
Komalpreet Kaur <komalpreetkaur05@edu.sait.ca>
2025-05-13 13:12:54 -0600

    working on week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return(
+        <main>
+            <h1>Student Info</h1>
+            <p>Student Name: komalpreet Kaur</p>
+            <Link href="https://github.com/komalpreetkaur05/cprg306-assignments.git">Github Repository Link</Link>
+        </main>
+    );
+}
